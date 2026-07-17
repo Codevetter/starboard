889c7d511835510e129ddb1349e35cc5b9371b10
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2026-07-17 13:32:50 +0530

    chore: remove unused lint suppression

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,6 @@ export default function RootLayout({
       <head>
         <script
           type="application/ld+json"
-          // biome-ignore lint/security/noDangerouslySetInnerHtml: static first-party JSON-LD
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               '@context': 'https://schema.org',
